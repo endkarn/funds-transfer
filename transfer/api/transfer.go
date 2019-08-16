c9d809d576dfe77ebcbcff105596d769c3495b32
endkarn <end_karn@hotmail.com>
2019-08-16 16:24:49 +0700

    [Created] Stub Function API ำหรับ  ATDD

--- transfer/api/transfer.go
+++ transfer/api/transfer.go
@@ -1,1 +1,29 @@
 package api
+
+import "github.com/gin-gonic/gin"
+
+func GetAccountDetail(c *gin.Context){
+	c.JSON(200, gin.H{
+		"number": 5732110628,
+		"balance":10000.00,
+	})
+}
+
+func PostTransfer(c *gin.Context){
+	c.JSON(200, gin.H{
+		"transaction_id": 1,
+		"fee":0,
+		"status":false,
+	})
+}
+
+func PostConfirmTransfer(c *gin.Context){
+	c.JSON(200, gin.H{
+		"transaction_id": 1,
+		"source_account_number":5732110628,
+		"destination_account_number":4351223508,
+		"amount":8500.00,
+		"fee":0,
+		"status":true,
+	})
+}
--- transfer/main.go
+++ transfer/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"flag"
+	"github.com/gin-gonic/gin"
+	"transfer/api"
+)
+
 func main() {
-	
+	host := flag.String("host", "3000", "your start host port")
+	flag.Parse()
+
+	r := gin.Default()
+	r.GET("/account/:account_number", api.GetAccountDetail)
+	r.POST("/transfer", api.PostTransfer)
+	r.POST("/confirm-transfer", api.PostConfirmTransfer)
+	r.Run(":"+*host)
 }
